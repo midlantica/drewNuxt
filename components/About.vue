dfaaff860e0a80e8ee584aa4f6baabc98b1d20f5
midlantica <druharper@gmail.com>
2024-01-31 12:30:50 -0600

    save

--- components/About.vue
+++ components/About.vue
@@ -144,7 +144,7 @@
     }
 
     .flag {
-      @apply block bg-no-repeat content-center items-center justify-center self-center w-[100px] h-[60px] bg-[url("/assets/img/brit_amer_flag.svg")] bg-contain mr-0 bg-center;
+      @apply block bg-no-repeat content-center items-center justify-center self-center w-[100px] h-[60px] bg-[url("/img/brit_amer_flag.svg")] bg-contain mr-0 bg-center;
     }
 
     p {
